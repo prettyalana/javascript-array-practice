6e7797e5e2eadd555d0fd6e9c55ef9adb354d479
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-05-29 17:42:22 -0500

    initial commit

---
+++ level-01/script.ts
@@ -0,0 +1,35 @@
+// 1.
+function createANewArray(string: string, count: number) {
+  let array: any[] = [];
+  for (let i = 0; i < count; i++) {
+    array.push(string);
+  }
+
+  return array;
+}
+
+const new_array = createANewArray("sunshine", 3);
+console.log(new_array);
+
+// 2.
+function reverseValues(array: any[]) {
+  const new_array = array.reverse();
+  return new_array;
+}
+
+const reversedValue = reverseValues([1, 2, 3, 4, 5]);
+console.log(reversedValue);
+
+// 3.
+function removeFalsyValues(array: any[]) {
+    const new_array: any[] = []
+    for (const element of array) {
+        if (element) {
+            new_array.push(element)
+        }
+    }
+    return new_array;
+}
+
+const trueValueArray = removeFalsyValues(['', "Alana", 1, null, 0]);
+console.log(trueValueArray);
